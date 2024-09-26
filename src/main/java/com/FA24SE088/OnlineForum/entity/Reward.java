446f05557e1a10a0fd090cd016aa4a4cad4c782d
omgSRT <thangckdt@gmail.com>
2024-09-26 20:24:11 +0700

    fix: reward entity missing field

--- src/main/java/com/FA24SE088/OnlineForum/entity/Reward.java
+++ src/main/java/com/FA24SE088/OnlineForum/entity/Reward.java
@@ -22,6 +22,8 @@ public class Reward {
     String name;
     String image;
     double price;
+    String type;
+    String status;
 
     @JsonIgnoreProperties(value = {"reward"}, allowSetters = true)
     @OneToMany(mappedBy = "reward", cascade = CascadeType.ALL, orphanRemoval = true)
