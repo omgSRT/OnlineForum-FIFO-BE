8dd9dfe5e9180163d2e84457dae91b89fe3383b7
huuphuc2001 <huuphuc3082001@gmail.com>
2024-09-26 09:06:01 +0700

    feat: account service

---
+++ src/main/java/com/FA24SE088/OnlineForum/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.FA24SE088.OnlineForum.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Slf4j
+@Service
+public class AccountService {
+}
