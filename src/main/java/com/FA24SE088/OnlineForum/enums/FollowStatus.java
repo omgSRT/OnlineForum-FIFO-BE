42c7e88623dde03d4ecfe2eec0131b6082b62284
huuphuc2001 <huuphuc3082001@gmail.com>
2024-10-05 10:22:15 +0700

    feat: follow service

---
+++ src/main/java/com/FA24SE088/OnlineForum/enums/FollowStatus.java
@@ -0,0 +1,7 @@
+package com.FA24SE088.OnlineForum.enums;
+
+public enum FollowStatus {
+    FOLLOWING,
+    UN_FOLLOW,
+    BLOCK,
+}
--- src/main/java/com/FA24SE088/OnlineForum/service/FollowService.java
+++ src/main/java/com/FA24SE088/OnlineForum/service/FollowService.java
@@ -1,5 +1,10 @@
 package com.FA24SE088.OnlineForum.service;
 
+import com.FA24SE088.OnlineForum.entity.Account;
+import com.FA24SE088.OnlineForum.entity.Follow;
+import com.FA24SE088.OnlineForum.enums.FollowStatus;
+import com.FA24SE088.OnlineForum.exception.AppException;
+import com.FA24SE088.OnlineForum.exception.ErrorCode;
 import com.FA24SE088.OnlineForum.repository.UnitOfWork.UnitOfWork;
 import com.FA24SE088.OnlineForum.utils.PaginationUtils;
 import lombok.AccessLevel;
@@ -7,7 +12,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
@@ -18,6 +28,29 @@ public class FollowService {
     UnitOfWork unitOfWork;
     @Autowired
     PaginationUtils paginationUtils;
+
+    private Account getCurrentUser(){
+        var context = SecurityContextHolder.getContext();
+        return unitOfWork.getAccountRepository().findByUsername(context.getAuthentication().getName()).orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+    }
+    public Follow create(UUID id) {
+        Account account = getCurrentUser();
+        Account account1 = unitOfWork.getAccountRepository().findById(id).orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+        return Follow.builder()
+                .follower(account)
+                .followee(account1)
+                .status(FollowStatus.FOLLOWING.name())
+                .build();
+    }
+
+    public List<String> unfollow(UUID id){
+        Account account = getCurrentUser();
+        Account account1 = unitOfWork.getAccountRepository().findById(id).orElseThrow(() -> new AppException(ErrorCode.ACCOUNT_NOT_FOUND));
+        List<Follow> list =  account.getFolloweeList().forEach(follow -> {
+
+        });
+    }
+
 
 
 }
