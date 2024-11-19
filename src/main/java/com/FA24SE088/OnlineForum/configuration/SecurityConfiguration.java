50296f8b40e8324232841154ed586efba3aded5b
huuphuc2001 <huuphuc3082001@gmail.com>
2024-11-19 22:38:37 +0700

    fix: cors

--- src/main/java/com/FA24SE088/OnlineForum/configuration/SecurityConfiguration.java
+++ src/main/java/com/FA24SE088/OnlineForum/configuration/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS_POST).permitAll()
                                 .requestMatchers(HttpMethod.PUT, PUBLIC_ENDPOINTS_PUT).permitAll()
                                 .requestMatchers("/ws/**", "/websocket/**", "websocket/app/**",
-                                        "/payment/**").permitAll()
+                                        "/payment/**","/**").permitAll()
                                 //.requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
