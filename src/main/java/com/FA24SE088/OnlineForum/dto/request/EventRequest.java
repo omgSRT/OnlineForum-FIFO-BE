eca1de92d0083375bf26e746cacb7e181298d6dc
huuphuc2001 <huuphuc3082001@gmail.com>
2024-11-28 16:44:17 +0700

    fix: event controller, payment service

--- src/main/java/com/FA24SE088/OnlineForum/dto/request/EventRequest.java
+++ src/main/java/com/FA24SE088/OnlineForum/dto/request/EventRequest.java
@@ -20,6 +20,7 @@ public class EventRequest {
     String location;
     @Column(columnDefinition = "MEDIUMTEXT")
     String image;
+    @Column(columnDefinition = "MEDIUMTEXT")
     String content;
     String link;
 //    String status;
